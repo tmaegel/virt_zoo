548e8fa7ddb67136c42377745cfb99cbb03fcb28
Toni Mägel <tmaegel@posteo.de>
2024-02-01 15:12:59 +0100

    feat(Pagination): Add TablePagination

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import CreateButton from "./components/CreateButton";
+import CreateEditDialog from "./components/CreateEditDialog";
+import DeleteButton from "./components/DeleteButton";
+import DeleteDialog from "./components/DeleteDialog";
+import Grid from "@mui/material/Grid";
+import NotificationBar from "./components/NotificationBar";
+import Paper from "@mui/material/Paper";
 import SearchField from "./components/SearchField";
 import Spinner from "./components/Spinner";
 import Table from "@mui/material/Table";
@@ -11,18 +18,13 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { Animal } from "./models/Animal";
 import { list, ApiError } from "./api/AnimalApi";
-import { useRouter } from "next/navigation";
-import DeleteDialog from "./components/DeleteDialog";
-import DeleteButton from "./components/DeleteButton";
-import NotificationBar from "./components/NotificationBar";
-import CreateButton from "./components/CreateButton";
-import Grid from "@mui/material/Grid";
-import CreateEditDialog from "./components/CreateEditDialog";
+import TablePagination from "@mui/material/TablePagination";
 
 export default function Home() {
-  const router = useRouter();
   const [data, setData] = useState<Animal[] | null>(null);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
   const [notification, setNotification] = useState<string | null>(null);
   const [create, setCreate] = useState(false);
   const [remove, setRemove] = useState<Animal | null>(null);
@@ -56,6 +58,15 @@ export default function Home() {
     setNotification("Erfolgreich gelöscht!");
   };
 
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   return (
     <div>
       {notification && <NotificationBar message={notification} onClose={() => setNotification(null)} />}
@@ -82,39 +93,50 @@ export default function Home() {
           <SearchField />
         </Grid>
         <Grid item xs={12}>
-          <TableContainer>
-            <Table aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell align="right">Gewicht (kg)</TableCell>
-                  <TableCell>Fähigkeit</TableCell>
-                  <TableCell>Ausgestorben seit (Jahre)</TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {data.map((obj) => (
-                  <TableRow
-                    key={obj.id!.toString()}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 }, cursor: "pointer" }}
-                    onClick={() => onEdit(obj)}
-                    hover={true}
-                  >
-                    <TableCell component="th" scope="row">
-                      {obj.name}
-                    </TableCell>
-                    <TableCell align="right">{obj.weight.toString()}</TableCell>
-                    <TableCell>{obj.capability}</TableCell>
-                    <TableCell>{obj.extinctSince.toString()}</TableCell>
-                    <TableCell align="right">
-                      <DeleteButton onRemove={() => setRemove(obj)} />
-                    </TableCell>
+          <Paper>
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell align="right">Gewicht (kg)</TableCell>
+                    <TableCell>Fähigkeit</TableCell>
+                    <TableCell>Ausgestorben seit (Jahre)</TableCell>
+                    <TableCell></TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+                </TableHead>
+                <TableBody>
+                  {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((obj) => (
+                    <TableRow
+                      key={obj.id!.toString()}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 }, cursor: "pointer" }}
+                      onClick={() => onEdit(obj)}
+                      hover={true}
+                    >
+                      <TableCell component="th" scope="row">
+                        {obj.name}
+                      </TableCell>
+                      <TableCell align="right">{obj.weight.toString()}</TableCell>
+                      <TableCell>{obj.capability}</TableCell>
+                      <TableCell>{obj.extinctSince.toString()}</TableCell>
+                      <TableCell align="right">
+                        <DeleteButton onRemove={() => setRemove(obj)} />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={data.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />{" "}
+          </Paper>
         </Grid>
       </Grid>
     </div>
