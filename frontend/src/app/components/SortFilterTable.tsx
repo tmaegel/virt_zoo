f9575180ae1e03336cd65d06ac35b011c9c818ff
Toni Mägel <tmaegel@posteo.de>
2024-02-02 08:34:59 +0100

    feat(SortFilterTable): Make table sortable

---
+++ frontend/src/app/components/SortFilterTable.tsx
@@ -0,0 +1,160 @@
+import { useState } from "react";
+import DeleteButton from "./DeleteButton";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import { Animal } from "../models/Animal";
+
+type Order = "asc" | "desc";
+
+function descending<K>(a: K, b: K) {
+  if (b < a) {
+    return -1;
+  }
+  if (b > a) {
+    return 1;
+  }
+  return 0;
+}
+
+function compare(a: Animal, b: Animal, orderBy: string) {
+  switch (orderBy) {
+    case "name":
+      return descending(a.name, b.name);
+    case "weight":
+      return descending(a.weight, b.weight);
+    case "capability":
+      return descending(a.capability, b.capability);
+    case "extinctSince":
+      return descending(a.extinctSince, b.extinctSince);
+    default:
+      return descending(a.name, b.name);
+  }
+}
+
+function getComparator(order: Order, orderBy: string): (a: Animal, b: Animal) => number {
+  return order === "desc" ? (a, b) => compare(a, b, orderBy) : (a, b) => -compare(a, b, orderBy);
+}
+
+type SortFilterTableProps = {
+  data: Animal[];
+  editHandler: (model: Animal) => void;
+  removeHandler: (model: Animal) => void;
+};
+
+type SortableTableHeadProps = {
+  order: Order;
+  orderBy: string;
+  onRequestSort: (event: React.MouseEvent<unknown>, property: string) => void;
+};
+
+export default function SortFilterTable({ data, editHandler, removeHandler }: SortFilterTableProps) {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [order, setOrder] = useState<Order>("asc");
+  const [orderBy, setOrderBy] = useState("name"); // id of tableHeadCells
+
+  const handleChangePage = (event: unknown, newPage: number) => setPage(newPage);
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const handleRequestSort = (e: React.MouseEvent<unknown>, property: string) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  return (
+    <Paper>
+      <TableContainer>
+        <Table>
+          <SortableTableHead order={order} orderBy={orderBy} onRequestSort={handleRequestSort} />
+          <TableBody>
+            {data
+              .slice()
+              .sort(getComparator(order, orderBy))
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((obj) => (
+                <TableRow
+                  key={obj.id!.toString()}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 }, cursor: "pointer" }}
+                  onClick={() => editHandler(obj)}
+                  hover={true}
+                >
+                  <TableCell component="th" scope="row">
+                    {obj.name}
+                  </TableCell>
+                  <TableCell>{obj.weight.toString()}</TableCell>
+                  <TableCell>{obj.capability}</TableCell>
+                  <TableCell>{obj.extinctSince.toString()}</TableCell>
+                  <TableCell align="right">
+                    <DeleteButton onRemove={() => removeHandler(obj)} />
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />{" "}
+    </Paper>
+  );
+}
+
+function SortableTableHead({ order, orderBy, onRequestSort }: SortableTableHeadProps) {
+  const createSortHandler = (property: string) => (event: React.MouseEvent<unknown>) => onRequestSort(event, property);
+
+  const tableHeadCells = [
+    {
+      id: "name",
+      label: "Name",
+    },
+    {
+      id: "weight",
+      label: "Gewicht (kg)",
+    },
+    {
+      id: "capability",
+      label: "Fähigkeit",
+    },
+    {
+      id: "Ausgestorben seit (Jahre)",
+      label: "extinctSince",
+    },
+  ];
+
+  return (
+    <TableHead>
+      <TableRow>
+        {tableHeadCells.map((headCell) => (
+          <TableCell key={headCell.id} sortDirection={orderBy === headCell.id ? order : false}>
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+        <TableCell></TableCell>
+      </TableRow>
+    </TableHead>
+  );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,28 +3,18 @@
 import { useState, useEffect } from "react";
 import CreateButton from "./components/CreateButton";
 import CreateEditDialog from "./components/CreateEditDialog";
-import DeleteButton from "./components/DeleteButton";
 import DeleteDialog from "./components/DeleteDialog";
 import Grid from "@mui/material/Grid";
 import NotificationBar from "./components/NotificationBar";
-import Paper from "@mui/material/Paper";
 import SearchField from "./components/SearchField";
+import SortFilterTable from "./components/SortFilterTable";
 import Spinner from "./components/Spinner";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
 import { Animal } from "./models/Animal";
 import { list, ApiError } from "./api/AnimalApi";
-import TablePagination from "@mui/material/TablePagination";
 
 export default function Home() {
   const [data, setData] = useState<Animal[] | null>(null);
   const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
   const [notification, setNotification] = useState<string | null>(null);
   const [create, setCreate] = useState(false);
   const [remove, setRemove] = useState<Animal | null>(null);
@@ -57,15 +47,6 @@ export default function Home() {
   const onRemove = (model: Animal) => {
     setData(data.filter((obj: Animal) => obj.id != model.id));
     setNotification("Erfolgreich gelöscht!");
-  };
-
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
   };
 
   return (
@@ -102,50 +83,7 @@ export default function Home() {
           <SearchField />
         </Grid>
         <Grid item xs={12}>
-          <Paper>
-            <TableContainer>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Name</TableCell>
-                    <TableCell align="right">Gewicht (kg)</TableCell>
-                    <TableCell>Fähigkeit</TableCell>
-                    <TableCell>Ausgestorben seit (Jahre)</TableCell>
-                    <TableCell></TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((obj) => (
-                    <TableRow
-                      key={obj.id!.toString()}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 }, cursor: "pointer" }}
-                      onClick={() => setEdit(obj)}
-                      hover={true}
-                    >
-                      <TableCell component="th" scope="row">
-                        {obj.name}
-                      </TableCell>
-                      <TableCell align="right">{obj.weight.toString()}</TableCell>
-                      <TableCell>{obj.capability}</TableCell>
-                      <TableCell>{obj.extinctSince.toString()}</TableCell>
-                      <TableCell align="right">
-                        <DeleteButton onRemove={() => setRemove(obj)} />
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <TablePagination
-              rowsPerPageOptions={[10, 25, 100]}
-              component="div"
-              count={data.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-            />{" "}
-          </Paper>
+          <SortFilterTable data={data} editHandler={setEdit} removeHandler={setRemove} />
         </Grid>
       </Grid>
     </div>
